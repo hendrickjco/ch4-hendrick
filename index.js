57ae3aa35f6f623271342242ec4958e397ce503c
hendrickjco <hendrikjco@gmail.com>
2021-06-28 21:38:39 +0900

    ku bingung

---
+++ index.js
@@ -0,0 +1,21 @@
+const c_rock = document.getElementById("rock-com");
+const c_paper = document.getElementById("paper-com");
+const c_scr = document.getElementById("scr-com");
+
+const p_rock = document.getElementById("rock-pl");
+const p_paper = document.getElementById("paper-pl");
+const p_scr = document.getElementById("scr-pl");
+
+c_rock.addEventListener("mouseover", setBGC);
+c_rock.addEventListener("mouseleave", removeBGC);
+c_rock.addEventListener("click");
+
+function setBGC() {
+    c_rock.style.backgroundColor = "red";
+    console.log("Com Rock");
+}
+
+function removeBGC() {
+    c_rock.style.backgroundColor = "";
+    console.log("bye com rock");
+}
